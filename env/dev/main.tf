14949c7a08d13861086f1c404003229ed0280f4f
skoochakpour <skoochakpour@myseneca.ca>
2025-08-13 08:57:20 -0400

    modify the module source

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,11 +1,11 @@
 module "network" {
-  source = "https://github.com/shivakpkp/terraform-azure-network"
+  source = "github.com/shivakpkp/terraform-azure-network"
 
   Vnet-name           = var.Vnet_name
   resource_group_name = var.resource_group_name
   location            = var.location
   address_space       = var.address_space
-  subnets             = var.subnet_name
+  subnets             = var.subnets
   nsg_name            = var.nsg_name
   security_rule       = var.security_rule
   }
